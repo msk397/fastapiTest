5894f031ee3900691542e244c47338eec93239f0
machangxin <41231040+machangxin@users.noreply.github.com>
2021-09-13 14:22:42 +0800

    Update database.py

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 # 使用mysqlclient的情况
-SQLALCHEMY_DATABASE_URL = "mysql://root:MChx199937@sh-cynosdbmysql-grp-rymc0t26.sql.tencentcdb.com:25970/property?charset=utf8"
-#sqlacodegen --outfile=models.py mysql://root:MChx199937@sh-cynosdbmysql-grp-rymc0t26.sql.tencentcdb.com:25970/property
+SQLALCHEMY_DATABASE_URL = "mysql://root:MC@sh-cynosdbmysql-grp-.sql.tencentcdb.com:250/property?charset=utf8"
+#sqlacodegen --outfile=models.py mysql://root:M@sh-cynosdbmysql-grp.sql.tencentcdb.com:20/property
 Base = declarative_base()
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
